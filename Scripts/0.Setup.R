7c2e0089cad78883622f4747d7a48435e1d1bfe2
Arno Botha <arno.spasie.botha@gmail.com>
2023-09-22 11:45:45 +0200

    Update 0.Setup.R

--- Scripts/0.Setup.R
+++ Scripts/0.Setup.R
@@ -61,23 +61,60 @@ sc.Thres <- 0.9; # repayment ratio - g1
 d <- 3 # default threshold for g0/g1-measures of delinquency (payments in arrears)
 k <- 6 # Probation period
 
-# - Custom path where R-scripts are saved
-path_cust <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/TruEnd-Procedure/Scripts/"
 
-# - Common path for storing important R-objects as back-up
-genObjPath <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/TruEnd-Procedure/Objects/"
-
-# - Common path for saving important analytics (e.g., sampling)
-genFigPath <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/TruEnd-Procedure/Figures/"
-# genFigPath <- "C:/Users/R5422965/OneDrive - FRG/TruEnd-Procedure/Figures/"
-# genFigPath <- "C:/TempData"
-
+# -- Path variables | General
 
 # - Common path for saving big data objects
 genPath <- "C:/Data/TruEnd-Procedure_Data/"
 
 # - Common path for importing raw data
 genRawPath <- "C:/Data/"
+
+
+# -- Path variables | User-dependent
+
+if (Sys.getenv("USERNAME") == "WRQ") {
+  
+  # - Custom path where R-scripts are saved
+  path_cust <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/TruEnd-Procedure/Scripts/"
+  
+  # - Common path for storing important R-objects as back-up
+  genObjPath <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/TruEnd-Procedure/Objects/"
+  
+  # - Common path for saving important analytics (e.g., sampling)
+  genFigPath <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/TruEnd-Procedure/Figures/"
+  
+} else if (Sys.getenv("USERNAME") == "Arno Botha") {
+  
+  # - Custom path where R-scripts are saved
+  path_cust <- "E:/WorkLife/Analytix/Research/TruEnd-Procedure/Scripts/"
+  
+  # - Common path for storing important R-objects as back-up
+  genObjPath <- "E:/WorkLife/Analytix/Research/TruEnd-Procedure/Objects/"
+  
+  # - Common path for saving important analytics (e.g., sampling)
+  genFigPath <- "E:/WorkLife/Analytix/Research/TruEnd-Procedure/Figures/"
+  
+  # - Common path for saving big data objects
+  genPath <- "E:/DataDump/FNB SLC/TruEnd-Procedure_Data/"
+  
+  # - Common path for importing raw data
+  genRawPath <- "E:/DataDump/FNB SLC/"
+  
+} else if (Sys.getenv("USERNAME") == "R5422965") {
+  
+  # - Custom path where R-scripts are saved
+  path_cust <- "C:/Users/R5422965/OneDrive - FRG/Analytix/Research/TruEnd-Procedure/Scripts/"
+  
+  # - Common path for storing important R-objects as back-up
+  genObjPath <- "C:/Users/R5422965/OneDrive - FRG/Analytix/Research/TruEnd-Procedure/Objects/"
+  
+  # - Common path for saving important analytics (e.g., sampling)
+  genFigPath <- "C:/Users/R5422965/OneDrive - FRG/Analytix/Research/TruEnd-Procedure/Figures/"
+  
+} else {
+  stop("User-specific paths not set for current user: ", Sys.getenv("USERNAME"), ". Please fix in Setup script (0.Setup.R) before continuing")
+}
 
 
 
